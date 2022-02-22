0af5a09f7a1f44c1de5f9576150865f0066091fa
Rbotti260 <99926319+Rbotti260@users.noreply.github.com>
2022-02-22 09:53:26 -0500

    Add files via upload

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Robburito");
+
+        int myFirstNumber = (10 + 5)+(2 * 10);
+        int mysecondnumber = 12;
+        int mythirdnumber = myFirstNumber * 2;
+        int mytotal = myFirstNumber+mysecondnumber+mythirdnumber;
+        System.out.println(mytotal);
+        int mylastone = 1000 - mytotal;
+        System.out.println(mylastone);
+
+
+
+    }
+}
